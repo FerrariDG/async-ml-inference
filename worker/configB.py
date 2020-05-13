284f0ca5ab3c34a97abf7ffff0d0ef43106462d1
Daniel Ferrari <ferrari.dg@gmail.com>
2020-05-13 16:30:18 +0100

    Add configurations for celery workers

---
+++ worker/configB.py
@@ -0,0 +1,10 @@
+"""."""
+from kombu import Queue
+
+task_acks_late = True
+
+worker_prefetch_multiplier = 1
+
+task_queues = [Queue(name="euro")]
+
+result_expires = 60 * 60 * 48  # 48 hours in seconds
